844367f122d2d6eabaac8488f7af78b310af8f99
Ruslan <r.salikhov96@gmail.com>
2020-02-24 23:57:21 +0500

    feat: add dream-team implementation

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    return Array.isArray(members) ? members.reduce((acc, cur) => {
+        if (typeof cur == 'string') { acc.push(/[a-zA-Z]/g.exec(cur)[0].toUpperCase()); }
+        return acc;
+    }, []).sort().join('') : false;
 };
