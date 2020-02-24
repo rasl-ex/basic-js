5e0cb943948dc345d58817d3d2102f303b54070d
Ruslan <r.salikhov96@gmail.com>
2020-02-24 07:12:31 +0500

    feat: add count-cats implementation

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,3 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    return matrix.reduce((acc1, cur1) => acc1 + cur1.reduce((acc2, cur2) => acc2 + (cur2 == "^^" ? 1 : 0), 0), 0);
 };
