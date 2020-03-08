2dd7ad44c17f0079a76eabbc7cb524af5727c9a8
Ruslan <r.salikhov96@gmail.com>
2020-03-09 00:36:53 +0500

    feat: add solution what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,12 +2,6 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-
-    //validation 
-
     if (typeof sampleActivity != 'string' || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY || /[a-zA-Z]/g.test(sampleActivity)) return false;
-
-    // clalculate carbon-dating
-
     return Math.ceil(Math.log((MODERN_ACTIVITY / parseFloat(sampleActivity))) / (0.693 / HALF_LIFE_PERIOD));
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date) {
+        return 'Unable to determine the time of year!';
+    }
+    if (Object.keys(date).length > 0) {
+        throw new Error();
+    }
+    let i = (date.getMonth());
+    if (i == 11 || i == 0 || i == 1) return ('winter');
+    if (i == 2 || i == 3 || i == 4) return ('spring');
+    if (i == 5 || i == 6 || i == 7) return ('summer');
+    if (i == 8 || i == 9 || i == 10) return ('autumn');
 };
