23cfb0ed00cca1ae996a5ffac38464236e1d604b
Ruslan <r.salikhov96@gmail.com>
2020-03-10 03:10:41 +0500

    feat: add solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,39 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    res: [],
+
+    getLength() {
+        return this.res.length;
+    },
+    addLink(value) {
+        this.res.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (this.res[position - 1] == undefined) {
+            this.res = [];
+            throw new Error("Error");
+        }
+        this.res.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.res.reverse();
+        return this;
+    },
+    finishChain() {
+        let result = "";
+        this.res.forEach((el, i) => {
+            if (i === 0) {
+                result += "( ";
+            }
+            result += el + " )~~( ";
+            if (i === this.res.length - 1) {
+                result = result.substring(0, result.length - 4);
+            }
+        });
+        this.res = [];
+        return result;
+    }
 };
 
 module.exports = chainMaker;
