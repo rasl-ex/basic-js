9daa327256935ec8619f24f6bc33ec00c36dc4fb
Ruslan <r.salikhov96@gmail.com>
2020-03-10 01:50:02 +0500

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let res = [];
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == "--discard-next") {
+                if (arr[i + 1] != undefined) {
+                    arr.splice(i, 1);
+                }
+            } else if (arr[i] == "--discard-prev") {
+                if (arr[i - 1] != undefined) {
+                    res.pop();
+                }
+            } else if (arr[i] == "--double-next") {
+                if (arr[i + 1] != undefined) {
+                    res.push(arr[i + 1]);
+                }
+            } else if (arr[i] == "--double-prev") {
+                if (arr[i - 1] != undefined) {
+                    res.push(arr[i - 1]);
+                }
+            } else {
+                res.push(arr[i]);
+            }
+        }
+        return (res);
+    } else {
+        throw new Error;
+    }
 };
