0099a487d45ed5c8aa3229e32732db73777343f7
Ruslan <r.salikhov96@gmail.com>
2020-03-12 02:17:22 +0500

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,61 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, { repeatTimes, separator = "+", addition, additionRepeatTimes, additionSeparator = "|" }) {
+    let additions = "";
+    let result = "";
+
+    if (addition === null) {
+        if (additionRepeatTimes != undefined) {
+            for (let i = 0; i < additionRepeatTimes; i++) {
+                if ((i + 1) != additionRepeatTimes) {
+                    additions += (addition + additionSeparator);
+                } else {
+                    additions += addition;
+                }
+            }
+        }
+        for (let j = 0; j < repeatTimes; j++) {
+            if (additions != undefined) {
+                if ((j + 1) != repeatTimes) {
+                    result += (str + additions + separator);
+                } else {
+                    result += str + additions;
+                }
+            } else if ((j + 1) != repeatTimes) {
+                result += (str + separator);
+            } else {
+                result += str;
+            }
+        }
+        return result;
+    }
+
+    if ((addition != undefined)) {
+        if (additionRepeatTimes != undefined) {
+            for (let i = 0; i < additionRepeatTimes; i++) {
+                if ((i + 1) != additionRepeatTimes) {
+                    additions += (addition + additionSeparator);
+                } else {
+                    additions += addition;
+                }
+            }
+        } else {
+            additions = str + addition;
+            return additions;
+        }
+    }
+
+    for (let j = 0; j < repeatTimes; j++) {
+        if (additions != undefined) {
+            if ((j + 1) != repeatTimes) {
+                result += (str + additions + separator);
+            } else {
+                result += str + additions;
+            }
+        } else if ((j + 1) != repeatTimes) {
+            result += (str + separator);
+        } else {
+            result += str;
+        }
+    }
+
+    return result;
 };
-  
