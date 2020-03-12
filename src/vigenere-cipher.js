e50f1676c46e0a38d328835464f26b60de3e207a
Ruslan <r.salikhov96@gmail.com>
2020-03-13 00:40:33 +0500

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,64 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(a = true) {
+        this.a = a;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error();
+        }
+
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        let encode = '';
+
+        while (key.length < message.length) {
+            key = key.concat(key);
+        }
+
+        let j = 0;
+
+        for (let i = 0; i < message.length; i++) {
+            if (/[A-Z]/.test(message[i])) {
+                encode += String.fromCodePoint((message[i].codePointAt() - 65 + key[j].codePointAt() - 65) % 26 + 65);
+                j++;
+            } else encode += message[i];
+        }
+
+        if (this.a) {
+            return encode;
+        } else {
+            return encode.split('').reverse().join('');
+        }
+    }
+
+    decrypt(text, key) {
+        if (!text || !key) {
+            throw new Error();
+        }
+
+        text = text.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        let decode = '';
+
+        while (key.length < text.length) {
+            key = key.concat(key);
+        }
+
+        let j = 0;
+
+        for (let i = 0; i < text.length; i++) {
+            if (/[A-Z]/.test(text[i])) {
+                decode += String.fromCodePoint((text[i].codePointAt() - key[j].codePointAt() + 26) % 26 + 65);
+                j++;
+            } else decode += text[i];
+        }
+
+        if (this.a) {
+            return decode;
+        } else {
+            return decode.split('').reverse().join('');
+        }
     }
 }
 
